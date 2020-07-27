8f0b8f5f04df649433bde081a15d02785a9ca975
Sherwinlove Bernardino <sherwinlove@Sherwinloves-Mac-Pro.local>
2020-07-27 13:25:22 -0700

    Updates

---
+++ precache-manifest.ad568bd5dfa26b27a68b50da5f2c1362.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88f2a7a1a54c9ff7a8ad4a00ed24f8e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fdaf5291c4c6b60a2bce",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "14fd178d550024b28fe9",
+    "url": "/static/js/2.e039f133.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.e039f133.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdaf5291c4c6b60a2bce",
+    "url": "/static/js/main.c260fe96.chunk.js"
+  },
+  {
+    "revision": "2dff72145c114e38c74d",
+    "url": "/static/js/runtime-main.489679c6.js"
+  }
+]);
