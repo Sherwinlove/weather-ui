b76272ff5aed68fc25e3460ae568f5fec7b88e10
Sherwinlove Bernardino <sherwinlove@Sherwinloves-Mac-Pro.local>
2020-07-27 13:49:12 -0700

    Updates

---
+++ precache-manifest.1fa1ff0d84d9f132f85db5231fce0824.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1d17e6cf1e13c3e0bfb024482035c02",
+    "url": "/weather-ui/index.html"
+  },
+  {
+    "revision": "519f8d7a29a78731af01",
+    "url": "/weather-ui/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bf0855fc7f273fdfb2a4",
+    "url": "/weather-ui/static/js/2.e08e1e95.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/weather-ui/static/js/2.e08e1e95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "519f8d7a29a78731af01",
+    "url": "/weather-ui/static/js/main.5e453425.chunk.js"
+  },
+  {
+    "revision": "99da8c1dd57df0c4366f",
+    "url": "/weather-ui/static/js/runtime-main.9ab07edd.js"
+  }
+]);
